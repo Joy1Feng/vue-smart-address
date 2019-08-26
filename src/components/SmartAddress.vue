9485cc46f4415b906524b5ec75aad4868e0d32fd
Joy <614106721@qq.com>
2019-08-26 18:03:32 +0800

    first commit

---
+++ src/components/SmartAddress.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <textarea @change="smart" cols="50" rows="10" v-model="info" placeholder="请粘贴你的地址"></textarea>
+    <div v-html="parseInfo"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'About',
+  components: {
+  },
+  data () {
+    return {
+      info: '',
+      parseInfo: ''
+    }
+  },
+  methods: {
+    smart () {
+      let parseList = parse(this.info)
+      let html = ''
+      for (let key in parseList) {
+        if (parseList[key]) {
+          html += `<p>` + key + `:` + parseList[key] + `</p>`
+        }
+      }
+      this.parseInfo = html
+    }
+  },
+  mounted () {
+    parseArea(areaList)
+  }
+}
+</script>
